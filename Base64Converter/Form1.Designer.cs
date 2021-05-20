2ca0ac4ae155add2e9e5ef2d741462c2b9098953
Isaac Bebawy <esaaco@live.com>
2021-05-20 12:24:57 +0200

    update

--- Base64Converter/Form1.Designer.cs
+++ Base64Converter/Form1.Designer.cs
@@ -41,12 +41,14 @@ namespace Base64Converter
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(521, 38);
+            this.button4.Location = new System.Drawing.Point(566, 13);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(92, 35);
             this.button4.TabIndex = 20;
@@ -56,9 +58,9 @@ namespace Base64Converter
             // 
             // btn_convert_tofile
             // 
-            this.btn_convert_tofile.Location = new System.Drawing.Point(468, 79);
+            this.btn_convert_tofile.Location = new System.Drawing.Point(566, 48);
             this.btn_convert_tofile.Name = "btn_convert_tofile";
-            this.btn_convert_tofile.Size = new System.Drawing.Size(145, 43);
+            this.btn_convert_tofile.Size = new System.Drawing.Size(92, 35);
             this.btn_convert_tofile.TabIndex = 19;
             this.btn_convert_tofile.Text = "Convert";
             this.btn_convert_tofile.UseVisualStyleBackColor = true;
@@ -67,40 +69,40 @@ namespace Base64Converter
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(78, 51);
+            this.label2.Location = new System.Drawing.Point(-2, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.Size = new System.Drawing.Size(111, 13);
             this.label2.TabIndex = 17;
-            this.label2.Text = "path";
+            this.label2.Text = "Path of converted file";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Location = new System.Drawing.Point(15, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.Size = new System.Drawing.Size(94, 13);
             this.label1.TabIndex = 18;
-            this.label1.Text = "file name with ext";
+            this.label1.Text = "File name with Ext";
             // 
             // txt_outputFile
             // 
             this.txt_outputFile.Font = new System.Drawing.Font("Tahoma", 11F);
-            this.txt_outputFile.Location = new System.Drawing.Point(113, 42);
+            this.txt_outputFile.Location = new System.Drawing.Point(113, 50);
             this.txt_outputFile.Name = "txt_outputFile";
-            this.txt_outputFile.Size = new System.Drawing.Size(402, 25);
+            this.txt_outputFile.Size = new System.Drawing.Size(447, 25);
             this.txt_outputFile.TabIndex = 15;
             // 
             // txt_file_ext
             // 
             this.txt_file_ext.Font = new System.Drawing.Font("Tahoma", 11F);
-            this.txt_file_ext.Location = new System.Drawing.Point(113, 13);
+            this.txt_file_ext.Location = new System.Drawing.Point(113, 23);
             this.txt_file_ext.Name = "txt_file_ext";
-            this.txt_file_ext.Size = new System.Drawing.Size(95, 25);
+            this.txt_file_ext.Size = new System.Drawing.Size(447, 25);
             this.txt_file_ext.TabIndex = 16;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(490, 3);
+            this.button3.Location = new System.Drawing.Point(486, 13);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(92, 35);
             this.button3.TabIndex = 14;
@@ -110,7 +112,7 @@ namespace Base64Converter
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(584, 3);
+            this.button2.Location = new System.Drawing.Point(580, 13);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(92, 35);
             this.button2.TabIndex = 13;
@@ -120,7 +122,7 @@ namespace Base64Converter
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(396, 4);
+            this.button1.Location = new System.Drawing.Point(392, 14);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(92, 35);
             this.button1.TabIndex = 11;
@@ -131,7 +133,7 @@ namespace Base64Converter
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Tahoma", 11F);
-            this.textBox1.Location = new System.Drawing.Point(71, 9);
+            this.textBox1.Location = new System.Drawing.Point(67, 19);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(321, 25);
             this.textBox1.TabIndex = 10;
@@ -139,43 +141,56 @@ namespace Base64Converter
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 13);
+            this.label3.Location = new System.Drawing.Point(8, 23);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.Size = new System.Drawing.Size(55, 13);
             this.label3.TabIndex = 18;
-            this.label3.Text = "Select file";
+            this.label3.Text = "Select File";
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.txt_outputFile);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.btn_convert_tofile);
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.txt_file_ext);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.txt_outputFile);
-            this.groupBox1.Location = new System.Drawing.Point(15, 47);
+            this.groupBox1.Location = new System.Drawing.Point(8, 77);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(661, 129);
+            this.groupBox1.Size = new System.Drawing.Size(678, 92);
             this.groupBox1.TabIndex = 21;
             this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(8, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(678, 72);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(691, 188);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "File Conversion";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -192,6 +207,7 @@ namespace Base64Converter
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
